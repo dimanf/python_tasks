b0e8d9c27ae72b86880382a7b754d754922857df
chak_CFH <you.die.in.2012@yandex.ru>
2015-06-03 21:11:48 +0500

    added method decorators

--- task2_0.py
+++ task2_0.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python
+# coding: utf-8
 
 #1.a
 class OldStyleClass():
@@ -10,9 +11,29 @@ class NewStyleClass(object):
 class AnotherNewStyleClass(NewStyleClass):
 	pass
 
-# 1.b 
 class ClassWithConstructor(object):
-	def __init__(self, first_arg, second_arg):
+	def __init__(self, *args):
 			# Автоматически задаем значения атрибутам класса при его создании.
-			self.first_arg = 'first'
-			self.second_arg = 'second'
+			if args:
+				self.first_arg = args[0]
+				self.second_arg = args[1]
+
+	@staticmethod
+	def smethod(first_arg, second_arg):
+		return first_arg + second_arg
+
+	@classmethod
+	def cmethod(cls):
+		return cls.__name__
+
+	@property
+	def cls_name(self):
+		return self.first_arg, self.second_arg
+
+class_with_staticmethod = ClassWithConstructor.smethod(1, 2)
+class_with_classmethod = ClassWithConstructor.cmethod()
+class_with_property = ClassWithConstructor('farg', 'sarg')
+
+# print class_with_staticmethod
+# print class_with_classmethod
+# print class_with_property.cls_name
